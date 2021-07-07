8ffb369e79018f4a4683cacdc5f801f0f90bcc0d
Nitesh167 <71694519+Nitesh167@users.noreply.github.com>
2021-07-07 13:02:30 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask
+from flask import json
+import logging
+
+
+logging.basicConfig(filename='example.log',level=logging.DEBUG)
+logging.debug('This message should go to the log file')
+logging.info('So should this')
+logging.warning('And this, too')
+logging.error('And non-ASCII stuff, too, like Øresund and Malmö')
+app = Flask(__name__)
+
+@app.route('/status')
+def healthcheck():
+    response = app.response_class(
+            response=json.dumps({"result":"OK - healthy"}),
+            status=200,
+            mimetype='application/json'
+    )
+
+    ## log line
+    app.logger.info('Status request successfull')
+    return response
+
+@app.route('/metrics')
+def metrics():
+    response = app.response_class(
+            response=json.dumps({"status":"success","code":0,"data":{"UserCount":140,"UserCountActive":23}}),
+            status=200,
+            mimetype='application/json'
+    )
+
+    ## log line
+    app.logger.info('Metrics request successfull')
+    return response
+
+@app.route("/")
+def hello():
+    ## log line
+    app.logger.info('Main request successfull')
+
+    return "Hello World!"
+
+if __name__ == "__main__":
+
+    ## stream logs to app.log file
+    #logging.basicConfig(filename='app.log',level=logging.DEBUG)
+
+    app.run(host='0.0.0.0')
